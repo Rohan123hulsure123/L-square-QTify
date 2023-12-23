b639b6668d2b4a05d9cfce0cf8048052557bd3c4
Rohan123hulsure123 <rohan.hulsure@gmail.com>
2023-12-23 18:15:41 +0530

    hero-section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import '@fontsource/poppins';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import HeroSection from './components/HeroSection/HeroSection';
 
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './HeroSection.css'
+export default function HeroSection() {
+  return (
+    <div className='hero-section'>
+        <h1>100 Thousand Songs, ad-free <br/> Over thousands podcast episodes </h1>
+        <img src='./vibrating-headphone 1.png' alt='hero-section'/>
+    </div>
+  )
+}
