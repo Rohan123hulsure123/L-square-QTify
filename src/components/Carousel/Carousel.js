0a42eac3787206ecaeb1bb0ffd8effa1a0db4784
Rohan123hulsure123 <rohan.hulsure@gmail.com>
2023-12-26 03:10:28 +0530

    carousel

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "./Carousel.css";
+import Card from "../Card/Card";
+
+function LeftButtom() {
+  return (
+    <div className="left-button">
+      <img src="./left.png" alt="left" />
+    </div>
+  );
+}
+function RightButtom() {
+  return (
+    <div className="right-button">
+      <img src="./right.png" alt="right" />
+    </div>
+  );
+}
+
+export default function Carousel({ albums }) {
+  return (
+    <div className="carousel">
+      <Swiper
+        
+        modules={[Navigation]}
+        spaceBetween={40}
+        slidesPerView={7}
+        navigation={{
+          prevEl: ".left-button",
+          nextEl: ".right-button",
+        }}
+        breakpoints={{
+            320: {
+              slidesPerView: 2,
+            },
+            480: {
+              slidesPerView: 3,
+            },
+            768: {
+              slidesPerView: 5,
+            },
+            1024: {
+              slidesPerView: 7,
+            },
+          }}
+      >
+        {/* <SwiperSlide>1</SwiperSlide>
+        <SwiperSlide>2</SwiperSlide>
+        <SwiperSlide>3</SwiperSlide>
+        <SwiperSlide>4</SwiperSlide>
+        <SwiperSlide>5</SwiperSlide>
+        <SwiperSlide>6</SwiperSlide>
+        <SwiperSlide>7</SwiperSlide>
+        <SwiperSlide>8</SwiperSlide>
+        <SwiperSlide>9</SwiperSlide>
+        <SwiperSlide>10</SwiperSlide>
+        <SwiperSlide>11</SwiperSlide>
+        <SwiperSlide>12</SwiperSlide>
+        <SwiperSlide>13</SwiperSlide> */}
+        {albums?.map((album) => {
+        return (
+          <SwiperSlide key={album.id}>
+            <Card
+              id={album.id}
+              title={album.title}
+              description={album.description}
+              follow_count={album.follows}
+              image={album.image}
+              slug={album.slug}
+              songs={album.songs}
+            />
+          </SwiperSlide>
+        );
+      })}
+      </Swiper>
+      <div className="slider-navigation">
+        <LeftButtom />
+        <RightButtom />
+      </div>
+    </div>
+  );
+}
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import "./Section.css"
 import Card from '../Card/Card';
+import Carousel from '../Carousel/Carousel';
 export default function Section({title, endpoint}) {
     const [albums, getalbum] = useState();
+    const [isCollapse, setIsCollapse] = useState(false);
     useEffect(()=>{
         async function fetchFunction() {
             const response = await fetch("https://qtify-backend-labs.crio.do/albums/"+endpoint)
@@ -13,16 +15,17 @@ export default function Section({title, endpoint}) {
     },[endpoint])
 
     // console.log(albums);
+    
   return (
     <div className='section-wrapper'>
         <div className='section-title-and-buttons'>
             {/* section heading and collapse */}
             <h4 className='section-heading'>{title}</h4>
-            <button className='section-button'>Collapse</button>
+            <button className='section-button' onClick={()=>setIsCollapse(!isCollapse)}>{isCollapse ? "Collapse" : "Show all"}</button>
         </div>
         <div className='section-cards-grid'>
             {/* cards */}
-            {
+            { !isCollapse ? <Carousel albums={albums}/> :
                 albums?.map(album =>{
                     return <Card key={album.id} id={album.id} title={album.title} description={album.description} follow_count={album.follows} image={album.image} slug={album.slug} songs={album.songs}/>
                 } )
